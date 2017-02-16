e5747fd929157c31e0bbf126b179d5db6b0de625
George Higbie <gehigbie@gmail.com>
2017-02-15 17:41:21 -0700

    added code to love.ejs file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-	res.send("<h1>WELCOME TO THE HOME PAGE!</h1>");
+	res.send("home.ejs");
 });
 
 app.get("/youhavetolove/:thing", function(req, res){
