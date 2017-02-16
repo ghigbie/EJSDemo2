92f6ae4fd04462448151c3f4179af24347da1791
George Higbie <gehigbie@gmail.com>
2017-02-15 17:32:15 -0700

    added specific path with variable

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", function(req, res){
 	res.send("<h1>WELCOME TO THE HOME PAGE!</h1>");
 });
 
+app.get("/youhavetolove/:thing", function(req, res){
+	var thing = req.params.thing.toLowerCase();
+	res.render()
+});
+
 app.listen(3300, function(){
 	console.log("The server has started to listen. Yay!!!");
 });
